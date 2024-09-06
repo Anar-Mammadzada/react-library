5623102b06bca8d46788652fd5b17fbc3cb019a7
Anar-Mammadzada <anarmammadza@gmail.com>
2024-09-06 15:17:46 +0400

    Add review list with pagination functionality

--- react-library/src/App.tsx
+++ react-library/src/App.tsx
@@ -10,6 +10,7 @@ import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
 import { Security, LoginCallback } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
+import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -34,6 +35,7 @@ export const App = () => {
             <Route path='/' element={<Navigate to="/home" />}></Route>
             <Route path='/home' element={<HomePage />}></Route>
             <Route path='/search' element={<SearchBooksPage />}></Route>
+            <Route path='/reviewList/:bookId' element={<ReviewListPage />}></Route>
             <Route path='/checkout/:bookId' element={<BookCheckoutPage />}></Route>
             <Route path='/login' element={< LoginWidget config={oktaConfig} />}></Route>
             <Route path='/login/callback' element={<LoginCallback />}></Route>
