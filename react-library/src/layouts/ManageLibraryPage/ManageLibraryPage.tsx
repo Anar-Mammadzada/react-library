e47b5ea8b87f90a0b55a7cb67ee8a668dd419969
Anar-Mammadzada <anarmammadza@gmail.com>
2024-10-14 21:06:13 +0400

    Add increase and decrease quantity of book, delete book functionality for administration

--- react-library/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ react-library/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { AdminMessages } from "./components/AdminMessages";
 import { AddNewBook } from "./components/AddNewBook";
+import { ChangeQuantityOfBooks } from "./components/ChangeQuantityOfBooks";
 
 export const ManageLibraryPage = () =>{
 
@@ -39,7 +40,7 @@ export const ManageLibraryPage = () =>{
                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
                         <button onClick={addBookClickFunction} className="nav-link active" id="nav-add-book-tab" data-bs-toggle="tab" 
                         data-bs-target="#nav-add-book" type="button" role="tab" aria-controls="nav-add-book" aria-selected="false">
-                            <AddNewBook />
+                            Add new book
                         </button>
                         <button onClick={changeQuantityOfBooksClickFunction} className="nav-link" id="nav-quantity-tab" data-bs-toggle="tab" 
                         data-bs-target="#nav-quantity" type="button" role="tab" aria-controls="nav-quantity" aria-selected="true">
@@ -53,10 +54,10 @@ export const ManageLibraryPage = () =>{
                 </nav>
                 <div className="tab-content" id="nav-tabContent">
                     <div className="tab-pane fade show active" id="nav-add-book" role="tabpanel" aria-labelledby="nav-add-book-tab">
-                        Add new book
+                    <AddNewBook />
                     </div>
                     <div className="tab-pane fade" id="nav-quantity" role="tabpanel" aria-labelledby="nav-quantity-tab">
-                        {changeQuantityOfBooksClick ? <>Change Quantity</> : <></>}
+                        {changeQuantityOfBooksClick ? <ChangeQuantityOfBooks /> : <></>}
                     </div>
                     <div className="tab-pane fade" id="nav-messages" role="tabpanel" aria-labelledby="nav-messages-tab">
                         {messagesClick ? <AdminMessages /> : <></>}
