0c6ab4d24b4f21bc361e162fc503d0f65e98055c
Anar-Mammadzada <anarmammadza@gmail.com>
2024-10-15 10:56:46 +0400

    Create HTTPS certificate and key, add HTTPS configuration in OKTA dashboard

--- react-library/src/lib/oktaConfig.ts
+++ react-library/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oaj0qo8eaJK91o9b5d7',
     issuer: 'https://dev-31694254.okta.com/oauth2/default',
-    redirectUri : 'http://localhost:3000/login/callback',
+    redirectUri : 'https://localhost:3000/login/callback',
     scopes : ['openid', 'profile', 'email'],
     pkce : true,
     disableHttpsCheck : true,
